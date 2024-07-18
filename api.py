a13648adb06afdb52dc8739e8ce056a70c2d5545
joshi.rohit <joshi.rohit@yahoo.com>
2024-07-18 09:42:38 -0400

    deepgram api test

--- api.py
+++ api.py
@@ -4,10 +4,17 @@
 from fastapi import FastAPI, Request, WebSocket 
 from fastapi.responses import JSONResponse, HTMLResponse
 from fastapi.templating import Jinja2Templates
+from typing import Dict, Callable
+
+from deepgram import Deepgram
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
+dg_client = Deepgram(os.getenv('DEEPGRAM_API_KEY'))
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
@@ -27,3 +34,26 @@ async def websocket_endpoint(websocket: WebSocket):
         raise Exception(f'Could not process audio: {e}')
     finally:
         await websocket.close()
+
+
+async def process_audio(fast_socket: WebSocket):
+    async def get_transcript(data: Dict) -> None:
+        if 'channel' in data:
+            transcript = data['channel']['alternatives'][0]['transcript']
+
+            if transcript:
+                await fast_socket.send_text(transcript)
+
+    deepgram_socket = await connect_to_deepgram(get_transcript)
+
+    return deepgram_socket
+
+async def connect_to_deepgram(transcript_received_handler: Callable[[Dict], None]):
+    try:
+        socket = await dg_client.transcription.live({'punctuate': True, 'interim_results': False})
+        socket.registerHandler(socket.event.CLOSE, lambda c: print(f'Connection closed with code {c}.'))
+        socket.registerHandler(socket.event.TRANSCRIPT_RECEIVED, transcript_received_handler)
+
+        return socket
+    except Exception as e:
+        raise Exception(f'Could not open socket: {e}')
